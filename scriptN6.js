593316a895cade0c5b6a4060751bcd87f99cad8d
AreoRut <valdsieg@gmail.com>
2019-10-27 23:29:14 +0500

    First commit

--- scriptN6.js
+++ scriptN6.js
@@ -1,6 +1,6 @@
 //document.querySelector('button').onclick = () => { }
 /////////////////////////////////////////////////////////////
-document.querySelector('button').onclick = myFunc;
+/*document.querySelector('button').onclick = myFunc;
 function myFunc() {
 }
 ////////////////////////////////////////////////////////////
@@ -13,7 +13,45 @@ document.querySelector('#button2').onclick = () => {
     } else {
         console.log("Не Нажат");
     }
-}
+}*/
 
 //for
 //while #Part2
+///////////////////////////////////////////////////////////
+let out = document.querySelector('.out');
+console.log(out);
+/*for (i = 0; i < 5; i++) {
+    //i=0;
+    for (let k = 0; k < 10; k++) {
+        //k=0;
+        out.innerHTML += "*";
+    }
+    out.innerHTML += '<br>';
+    //
+}*/
+//////////////////////////////////////////////////////////////
+
+
+for (let i = 1; i < 10; i++) {
+    //out.innerHTML += '3*' + i + '=' + (i * 3) + '<br>';
+    //out.innerHTML += `3*${i}=${3 * i}<br>`;
+
+    for (let k = 1; k < 10; k++) {
+        //out.innerHTML += `3*${i}=${3 * i}<br>`;
+        out.innerHTML += `${i}*${k}=${k * i}<br>`;
+    }
+    out.innerHTML += '<hr>';
+}
+
+//////////////////////////////////////////////////////////
+//#1
+
+for (i = 0; i < 3; i++) {
+    //i=0;
+    for (let k = 0; k < 3; k++) {
+
+        out.innerHTML += "*";
+    }
+    out.innerHTML += '_';
+
+}
